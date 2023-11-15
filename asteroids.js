6905034397f177499a52a8384dd7d356e63cdb2b
Dodica <occupied.fall@gmail.com>
2023-11-15 14:05:17 +0100

    more comments

--- asteroids.js
+++ asteroids.js
@@ -39,112 +39,134 @@ function Drawable(texture, position, scale) {
 	this.position = position;
 	this.width = texture.width;
 	this.height = texture.height;
-  
+
 	// Ako je definiran parametar skale, postavi širinu i visinu u skladu s tom skalom
 	if (scale) {
-	  this.scaleWidth = this.width * scale;
-	  this.scaleHeight = this.height * scale;
+		this.scaleWidth = this.width * scale;
+		this.scaleHeight = this.height * scale;
 	}
-  }
-  
-  // Metoda za crtanje objekta na određenom kontekstu (canvas)
-  Drawable.prototype.draw = function (context) {
+}
+
+// Metoda za crtanje objekta na određenom kontekstu (canvas)
+Drawable.prototype.draw = function (context) {
 	// Ako nije definirana širina skaliranja, nacrtaj objekt bez skaliranja
 	if (!this.scaleWidth) {
-	  context.drawImage(
-		this.texture,
-		Math.round(this.position.x - this.width / 2),
-		Math.round(this.position.y - this.height / 2)
-	  );
+		context.drawImage(
+			this.texture,
+			Math.round(this.position.x - this.width / 2),
+			Math.round(this.position.y - this.height / 2)
+		);
 	} else { // Inače, nacrtaj skalirani objekt
-	  context.drawImage(
-		this.texture,
-		0,
-		0,
-		this.width,
-		this.height,
-		Math.round(this.position.x - this.scaleWidth / 2),
-		Math.round(this.position.y - this.scaleHeight / 2),
-		this.scaleWidth,
-		this.scaleHeight
-	  );
+		context.drawImage(
+			this.texture,
+			0,
+			0,
+			this.width,
+			this.height,
+			Math.round(this.position.x - this.scaleWidth / 2),
+			Math.round(this.position.y - this.scaleHeight / 2),
+			this.scaleWidth,
+			this.scaleHeight
+		);
 	}
-  };
+};
 
 // Funkcija koja predstavlja objekt koji se može kretati (npr. igrač, projektil)
 function Moveable(texture, position, direction, angle) {
 	// Pozivamo konstruktor roditeljske klase (Drawable) s određenim parametrima
 	Drawable.call(this, texture, position);
-  
+
 	// Spremamo smjer i kut kretanja objekta
 	this.direction = direction;
 	this.angle = angle;
-  }
-
-// Postavljanje prototipa za Moveable kao podobjekta od Drawable.
-Moveable.prototype = Object.create(Drawable.prototype);
-
-// Metoda za crtanje objekta Moveable na zadanom kontekstu.
-Moveable.prototype.draw = function (context) {
-    context.translate(this.position.x, this.position.y);
-    context.rotate(this.angle);
-    context.drawImage(this.texture, -Math.round(this.width / 2), -Math.round(this.height / 2));
-    context.rotate(-this.angle);
-    context.translate(-this.position.x, -this.position.y);
-};
-
-// Metoda za omotavanje granica, osiguravajući da objekt ostane unutar ekrana.
-Moveable.prototype.wrapBoundry = function () {
-    var x = this.position.x;
-    var y = this.position.y;
-    var width = this.width / 2;
-    var height = this.height / 2;
-
-    if (x > maxX + width) {
-        x = minX - width;
-    } else if (x < minX - width) {
-        x = maxX + width;
-    } else if (y > maxY + height) {
-        y = minY - height;
-    } else if (y < minY - height) {
-        y = maxY + height;
-    }
-
-    this.position = new Vector(x, y);
-};
-
-// Metoda za provjeru preklapanja objekta Moveable s drugim objektom Drawable.
-Moveable.prototype.intersects = function (drawable) {
-    var leftX = this.position.x - this.width / 2;
-    var topY = this.position.y - this.height / 2;
-    var rightX = leftX + this.width;
-    var bottomY = topY + this.height;
-
-    var dLeftX = drawable.position.x - drawable.width / 2;
-    var dTopY = drawable.position.y - drawable.height / 2;
-    var dRightX = dLeftX + drawable.width;
-    var dBottomY = dTopY + drawable.height;
-
-    return rightX > dLeftX && leftX < dRightX && bottomY > dTopY && topY < dBottomY;
-};
-
-// Konstruktor za objekt Ship koji nasljeđuje od Moveable. Postavlja teksturu, poziciju, smjer i kut.
-function Ship(position, angle) {
-	Moveable.call(this, imageRepo.ship, position, new Vector(Math.cos(angle), Math.sin(angle)), angle);
-	this.width = 69; // Širina broda.
-	this.speed = new Vector(0, 0); // Brzina broda.
-	this.fireRate = 10; // Brzina ispaljivanja.
-	this.fireCounter = 0; // Brojač ispaljivanja.
 }
 
-// Postavljanje prototipa za Ship kao podobjekta od Moveable.
+// Postavljanje prototipa za Moveable kao podobjekta od Drawable
+Moveable.prototype = Object.create(Drawable.prototype);
+
+// Metoda za crtanje objekta Moveable na zadanom kontekstu
+Moveable.prototype.draw = function (context) {
+	// Postavi trenutnu poziciju kao središte transformacije
+	context.translate(this.position.x, this.position.y);
+
+	// Rotiraj objekt prema zadanim kutem
+	context.rotate(this.angle);
+
+	// Nacrtaj teksturu objekta na kontekstu
+	context.drawImage(this.texture, -Math.round(this.width / 2), -Math.round(this.height / 2));
+
+	// Ponovno postavi poziciju kao središte transformacije natrag na početnu
+	context.rotate(-this.angle);
+	context.translate(-this.position.x, -this.position.y);
+};
+
+// Metoda za omotavanje granica, osiguravajući da objekt ostane unutar ekrana
+Moveable.prototype.wrapBoundry = function () {
+	// Spremi trenutne koordinate objekta
+	var x = this.position.x;
+	var y = this.position.y;
+
+	// Izračunaj polovicu širine i visine objekta
+	var width = this.width / 2;
+	var height = this.height / 2;
+
+	// Provjeri uvjete za omotavanje granica po X i Y osima
+	if (x > maxX + width) {
+		// Ako je objekt izvan desne granice, postavi ga na lijevu stranu ekrana
+		x = minX - width;
+	} else if (x < minX - width) {
+		// Ako je objekt izvan lijeve granice, postavi ga na desnu stranu ekrana
+		x = maxX + width;
+	} else if (y > maxY + height) {
+		// Ako je objekt izvan donje granice, postavi ga na gornju stranu ekrana
+		y = minY - height;
+	} else if (y < minY - height) {
+		// Ako je objekt izvan gornje granice, postavi ga na donju stranu ekrana
+		y = maxY + height;
+	}
+
+	// Postavi nove koordinate objekta
+	this.position = new Vector(x, y);
+};
+
+// Metoda za provjeru preklapanja objekta Moveable s drugim objektom Drawable
+Moveable.prototype.intersects = function (drawable) {
+	// Definiraj granice (lijevu, gornju, desnu i donju) trenutnog objekta
+	var leftX = this.position.x - this.width / 2;
+	var topY = this.position.y - this.height / 2;
+	var rightX = leftX + this.width;
+	var bottomY = topY + this.height;
+
+	// Definiraj granice (lijevu, gornju, desnu i donju) drugog objekta (Drawable)
+	var dLeftX = drawable.position.x - drawable.width / 2;
+	var dTopY = drawable.position.y - drawable.height / 2;
+	var dRightX = dLeftX + drawable.width;
+	var dBottomY = dTopY + drawable.height;
+
+	// Provjeri preklapanje po X i Y osima te vrati rezultat
+	return rightX > dLeftX && leftX < dRightX && bottomY > dTopY && topY < dBottomY;
+};
+
+// Konstruktor za objekt Ship koji nasljeđuje od Moveable. Postavlja teksturu, poziciju, smjer i kut
+function Ship(position, angle) {
+	Moveable.call(this, imageRepo.ship, position, new Vector(Math.cos(angle), Math.sin(angle)), angle);
+	this.width = 69; // Širina broda
+	this.speed = new Vector(0, 0); // Brzina broda
+	this.fireRate = 10; // Brzina ispaljivanja
+	this.fireCounter = 0; // Brojač ispaljivanja
+}
+
+// Postavljanje prototipa za Ship kao podobjekta od Moveable
 Ship.prototype = Object.create(Moveable.prototype);
 
-// Metoda za crtanje broda na zadanom kontekstu.
+// Metoda za crtanje broda na zadanom kontekstu
 Ship.prototype.draw = function (context) {
+	// Postavi poziciju i kut rotacije broda
 	context.translate(this.position.x, this.position.y);
 	context.rotate(this.angle);
-	var offset = KEY_STATUS.up && life > 0 ? this.width : 0; // Podešavanje offseta za animaciju broda.
+	var offset = KEY_STATUS.up && life > 0 ? this.width : 0; // Odredi offset za animaciju broda ako je pritisnuta tipka "Gore" i igrač je živ
+
+	// Nacrtaj brod na određenom kontekstu
 	context.drawImage(
 		this.texture,
 		offset,
@@ -156,98 +178,100 @@ Ship.prototype.draw = function (context) {
 		this.width,
 		this.height
 	);
-	context.rotate(-this.angle);
-	context.translate(-this.position.x, -this.position.y);
+
+	context.rotate(-this.angle);// Vrati kut rotacije na prethodnu vrijednost
+
+	context.translate(-this.position.x, -this.position.y);// Vrati poziciju na prethodnu vrijednost
 };
 
 // Metoda za kretanje broda.
 Ship.prototype.move = function () {
-	if (!isAlive) return;
+	if (!isAlive) return; // ako nije živ vrati odmah
 	this.fireCounter++;
 	this.position.x += this.speed.x;
 	this.position.y += this.speed.y;
-	this.wrapBoundry(); // Provjera i omotavanje granica broda.
+	this.wrapBoundry(); // Provjera i omotavanje granica broda
 
-	for (var i = 0; i < asteroids.length; i++) {
-		if (this.intersects(asteroids[i])) {
-			// Dodavanje eksplozije na sredinu između broda i asteroida.
+	for (var i = 0; i < asteroids.length; i++) { // Za svaki asteroid
+		if (this.intersects(asteroids[i])) { // Ako se brod i asteroid sudare
+			// Dodavanje eksplozije na sredinu između broda i asteroida
 			explosions.push(new Explosion(new Vector((this.position.x + asteroids[i].position.x) / 2,
-				(this.position.y + asteroids[i].position.y) / 2), true));
-			soundRepo.play('bigExplosion');
-			asteroids.splice(i, 1); // Uklanjanje asteroida.
+				(this.position.y + asteroids[i].position.y) / 2), true)); // Dodaj novu ekploziju
+			soundRepo.play('bigExplosion'); // Zvuk eksplozije
+			asteroids.splice(i, 1); // Uklanjanje asteroida
 			isAlive = false;
 			life--;
-			var currentTime = new Date();
-			if (currentTime - startTime > bestTime) {
+			var currentTime = new Date(); // Trenutno vrijeme
+			if (currentTime - startTime > bestTime) { // Proteklo vrijeme je trenutno vrijeme - početno vrijeme
 				bestTime = currentTime - startTime;
-				localStorage.setItem('best_time', bestTime); // Ažuriranje najboljeg vremena u lokalnom pohranjivanju.
+				localStorage.setItem('best_time', bestTime); // Ažuriranje najboljeg vremena u lokalnom pohranjivanju
 			}
 			break;
 		}
 	}
 };
 
-// Metoda za upravljanje brodom.
+// Metoda za upravljanje brodom
 Ship.prototype.control = function () {
 	if (!isAlive) return;
 	if (KEY_STATUS.right)
-		this.angle += .08; // Rotacija udesno.
+		this.angle += .08; // Rotacija udesno
 	else if (KEY_STATUS.left)
-		this.angle -= .08; // Rotacija ulijevo.
+		this.angle -= .08; // Rotacija ulijevo
 
-	this.angle %= circle; // Osiguravanje da kut ostane u granicama kruga.
+	this.angle %= circle; // Osiguravanje da kut ostane u granicama kruga
 	this.direction = new Vector(Math.cos(this.angle), Math.sin(this.angle));
 
 	if (KEY_STATUS.up) {
 		if (this.direction.x > 0 && this.speed.x < 10 ||
 			this.direction.x < 0 && this.speed.x > -10)
-			this.speed.x += this.direction.x * .1; // Povećanje brzine prema naprijed.
+			this.speed.x += this.direction.x * .1; // Povećanje brzine prema naprijed u x smjeru
 		if (this.direction.y > 0 && this.speed.y < 10 ||
 			this.direction.y < 0 && this.speed.y > -10)
-			this.speed.y += this.direction.y * .1; // Povećanje brzine prema naprijed.
+			this.speed.y += this.direction.y * .1; // Povećanje brzine prema naprijed u y smjeru
 	}
 	else if (KEY_STATUS.down) {
 		if (this.speed.x > .2 || this.speed.x < -.2 ||
 			this.speed.y > .2 || this.speed.y < -.2) {
-			this.speed.x *= .9; // Smanjenje brzine.
-			this.speed.y *= .9; // Smanjenje brzine.
+			this.speed.x *= .9; // Smanjenje brzine u x smjeru
+			this.speed.y *= .9; // Smanjenje brzine u y smjeru
 		}
 		else
-			this.speed = new Vector(0, 0); // Brzina 0 ako nema pritiska na tipku.
+			this.speed = new Vector(0, 0); // Brzina 0 ako nema pritiska na tipku
 	}
 	if (KEY_STATUS.space && this.fireCounter > this.fireRate) {
-		// Iscrtavanje i dodavanje nove vatrenke.
+		// Iscrtavanje i dodavanje novih vatrenih kugli
 		var position = new Vector(this.position.x + this.direction.x * 30, this.position.y + this.direction.y * 30);
-		fireballs.push(new Moveable(imageRepo.fireball, position, this.direction, this.angle));
-		soundRepo.play('fire');
-		this.fireCounter = 0; // Resetiranje brojača ispaljivanja.
+		fireballs.push(new Moveable(imageRepo.fireball, position, this.direction, this.angle)); // Stvori nove vatrene kugle
+		soundRepo.play('fire'); // Zvuk ispaljivanja
+		this.fireCounter = 0; // Resetiranje brojača ispaljivanja
 	}
 };
 
-// Konstruktor za objekt Asteroid koji nasljeđuje od Moveable.
+// Konstruktor za objekt Asteroid koji nasljeđuje od Moveable
 function Asteroid(position, direction, angle, rotation, isBig) {
 	Moveable.call(this, imageRepo[isBig ? 'asteroidBig' : 'asteroidSmall'], position, direction, angle);
-	this.rotation = rotation; // Postavljanje rotacije asteroida.
+	this.rotation = rotation; // Postavljanje rotacije asteroida
 }
 
-// Postavljanje prototipa za Asteroid kao podobjekta od Moveable.
+// Postavljanje prototipa za Asteroid kao podobjekta od Moveable
 Asteroid.prototype = Object.create(Moveable.prototype);
 
-// Konstruktor za objekt Explosion koji nasljeđuje od Drawable.
+// Konstruktor za objekt Explosion koji nasljeđuje od Drawable
 function Explosion(position, isBig) {
 	Drawable.call(this, imageRepo[isBig ? 'explosionBig' : 'explosionSmall'], position);
 	this.width = isBig ? 100 : 64; // Postavljanje širine eksplozije.
 	this.life = 0; // Inicijalizacija životnog vijeka eksplozije.
 }
 
-// Postavljanje prototipa za objekt Explosion kao podobjekta od Drawable.
+// Postavljanje prototipa za objekt Explosion kao podobjekta od Drawable
 Explosion.prototype = Object.create(Drawable.prototype);
 
 // Definicija metode draw za objekt Explosion.
 Explosion.prototype.draw = function (context) {
-	var spriteX = this.width * this.life; // Izračunavanje x-koordinate trenutnog okvira animacije.
+	var spriteX = this.width * this.life; // Izračunavanje x-koordinate trenutnog okvira animacije
 
-	// Crta eksploziju koristeći trenutni okvir animacije.
+	// Crta eksploziju koristeći trenutni okvir animacije
 	context.drawImage(
 		this.texture,
 		spriteX,
@@ -260,23 +284,23 @@ Explosion.prototype.draw = function (context) {
 		this.height
 	);
 
-	this.life++; // Povećava životni vijek eksplozije za idući okvir animacije.
+	this.life++; // Povećava životni vijek eksplozije za idući okvir animacije
 };
 
 // Repozitorij slika
 var imageRepo = (function () {
-	var numImages = 10; // Ukupan broj slika koje se učitavaju.
-	var numLoaded = 0; // Broj trenutno učitanih slika.
+	var numImages = 10; // Ukupan broj slika koje se učitavaju
+	var numLoaded = 0; // Broj trenutno učitanih slika
 
-	// Funkcija koja se poziva kada se slika učita, povećava broj učitanih slika i inicijalizira prozor ako su sve slike učitane.
+	// Funkcija koja se poziva kada se slika učita, povećava broj učitanih slika i inicijalizira prozor ako su sve slike učitane
 	function imageLoaded() {
 		numLoaded++;
 		if (numLoaded === numImages) {
-			window.init(); // Poziv inicijalizacijske funkcije kad su sve slike učitane.
+			window.init(); // Poziv inicijalizacijske funkcije kad su sve slike učitane
 		}
 	}
 
-	// Objekt koji sadrži putanje do slika i funkciju za njihovo učitavanje.
+	// Objekt koji sadrži putanje do slika i funkciju za njihovo učitavanje
 	return {
 		background: loadImage('img/background.png'),
 		title: loadImage('img/title.png'),
@@ -290,51 +314,51 @@ var imageRepo = (function () {
 		explosionBig: loadImage('img/explosionbig.png'),
 	};
 
-	// Funkcija koja kreira novi Image objekt, postavlja mu izvor slike, dodaje slušatelj za događaj učitavanja i vraća stvoreni objekt.
+	// Funkcija koja kreira novi Image objekt, postavlja mu izvor slike, dodaje slušatelj za događaj učitavanja i vraća stvoreni objekt
 	function loadImage(src) {
 		var image = new Image();
 		image.src = src;
-		image.onload = imageLoaded; // Dodavanje slušatelja događaja učitavanja slike.
-		return image; // Vraćanje stvorenog Image objekta.
+		image.onload = imageLoaded; // Dodavanje slušatelja događaja učitavanja slike
+		return image; // Vraćanje stvorenog Image objekta
 	}
 })();
 
 // Repozitorij zvukova
 var soundRepo = (function () {
-	var size = 10; // Veličina zvučnog bazena, odnosno broj zvukova u bazenu.
+	var size = 10; // Veličina zvučnog bazena, odnosno broj zvukova u bazenu
 
-	// Stvaranje bazena zvukova za paljenje vatre.
+	// Stvaranje bazena zvukova za paljenje vatre
 	var firePool = createSoundPool('audio/fire.mp3');
-	// Stvaranje bazena zvukova za eksplozije.
+	// Stvaranje bazena zvukova za eksplozije
 	var explosionPool = createSoundPool('audio/explosion.mp3');
-	// Stvaranje bazena zvukova za velike eksplozije.
+	// Stvaranje bazena zvukova za velike eksplozije
 	var bigExplosionPool = createSoundPool('audio/explosionbig.mp3');
 
-	// Funkcija za stvaranje bazena zvukova.
+	// Funkcija za stvaranje bazena zvukova
 	function createSoundPool(src) {
 		var pool = { current: 0, sounds: [] };
 		for (var i = 0; i < size; i++) {
-			var audio = new Audio(src); // Stvaranje novog Audio objekta za zvuk.
-			audio.load(); // Učitavanje zvuka.
-			pool.sounds.push(audio); // Dodavanje zvuka u bazen.
+			var audio = new Audio(src); // Stvaranje novog Audio objekta za zvuk
+			audio.load(); // Učitavanje zvuka
+			pool.sounds.push(audio); // Dodavanje zvuka u bazen
 		}
-		return pool; // Vraćanje stvorenog bazena zvukova.
+		return pool; // Vraćanje stvorenog bazena zvukova
 	}
 
-	// Funkcija za reprodukciju zvuka iz određenog bazena.
+	// Funkcija za reprodukciju zvuka iz određenog bazena
 	function play(sound) {
-		// Odabir bazena zvukova na temelju naziva zvuka.
+		// Odabir bazena zvukova na temelju naziva zvuka
 		var soundPool = (sound === 'fire') ? firePool : (sound === 'explosion') ? explosionPool : bigExplosionPool;
 
 		var currentSound = soundPool.sounds[soundPool.current];
-		// Reprodukcija zvuka samo ako nije trenutno u reprodukciji.
+		// Reprodukcija zvuka samo ako nije trenutno u reprodukciji
 		if (currentSound.currentTime === 0 || currentSound.ended) {
 			currentSound.play();
 		}
-		soundPool.current = (soundPool.current + 1) % size; // Pomicanje indeksa trenutnog zvuka u bazenu.
+		soundPool.current = (soundPool.current + 1) % size; // Pomicanje indeksa trenutnog zvuka u bazenu
 	}
 
-	// Objekt koji sadrži funkciju za reprodukciju zvuka.
+	// Objekt koji sadrži funkciju za reprodukciju zvuka
 	return {
 		play: play
 	};
@@ -387,7 +411,7 @@ function displayMenu() {
 }
 
 function animate() {
-	// Poziva funkciju za animaciju na sljedećem frejmu
+	// Poziva funkciju za animaciju na sljedećem frame-u
 	requestAnimFrame(animate);
 
 	// Provjerava je li trenutna stranica naslovnica ili igra
@@ -406,8 +430,9 @@ function handleTitlePage() {
 		// Briše sadržaj iz meni konteksta
 		menuContext.clearRect(0, 0, maxX, maxY);
 
-		// Postavlja naslovnicu na "false" i bilježi trenutno vrijeme
+		// Postavlja naslovnicu na "false"
 		titlePage = false;
+		// Pamti trenutno vrijeme za izračun najboljeg vremena
 		startTime = new Date();
 	}
 }
@@ -421,7 +446,7 @@ function handleGamePage() {
 		ship.draw(actionContext);
 	}
 
-	// Pomakni brod
+	// Pomicanje broda
 	ship.move();
 
 	// Crta meteore, vatrenke i eksplozije
@@ -563,7 +588,7 @@ function randomAsteroid(posX, posY, isBig) {
 	asteroidRotation *= -1;
 }
 
-// Funkcija za crtanje vatrenih kugli (fireballs)
+// Funkcija za crtanje vatrenih kugli
 function drawFireballs() {
 	// Dimenzije vatrene kugle
 	const fireballWidth = imageRepo.fireball.width;
@@ -607,11 +632,12 @@ function drawFireballs() {
 						explosions.push(new Explosion(asteroids[x].position, true));
 						soundRepo.play('bigExplosion');
 
-						// Stvaranje četiri manja asteroida
+						// Stvaranje četiri manja asteroida iz većeg koji je uništen
 						for (let a = 0; a < 4; a++) {
 							randomAsteroid(asteroids[x].position.x, asteroids[x].position.y, false);
 						}
 					} else {
+						// Stvara eksploziju
 						explosions.push(new Explosion(asteroids[x].position, false));
 						soundRepo.play('explosion');
 					}
